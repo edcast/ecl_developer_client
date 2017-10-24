335fa4b696cff1ebea3cd32ed4efcd8041f12945
Amar Daxini <amardaxini@gmail.com>
2017-10-24 14:34:48 +0530

    EP-7731 fixed commumicatior

--- lib/ecl_developer_client/base_communicator.rb
+++ lib/ecl_developer_client/base_communicator.rb
@@ -1,70 +1,71 @@
+module EclDeveloperClient
+  class BaseCommunicator
+    attr_accessor :response_data,:token,:client_id,:payload
 
-class BaseCommunicator
-  attr_accessor :response_data,:token,:client_id,:payload
+    def initialize(client_id,token,payload={})
+      @payload = payload
+      @token = token
+      @client_id = client_id
+    end
 
-  def initialize(client_id,token,payload={})
-    @payload = payload
-    @token = token
-    @client_id = client_id
-  end
+    def headers
+      {
+        "X-Api-Token" => @token,
+        "X-Api-Client" => @client_id
+      }
+    end
 
-  def headers
-    {
-      "X-Api-Token" => @token,
-      "X-Api-Client" => @client_id
-    }
-  end
+   
+    def base_url
+      EclDeveloperClient.configuration.ecl_app_url
+    end
 
- 
-  def base_url
-    EclClient.configuration.ecl_app_url
-  end
+   
+    def establish_connection(relative_url, params={})
+      conn = Faraday.new(base_url)
+      @response = conn.get do |req|
+        req.url relative_url
+        req.headers = headers
+        req.params = params
+      end
+    end
 
- 
-  def establish_connection(relative_url, params={})
-    conn = Faraday.new(base_url)
-    @response = conn.get do |req|
-      req.url relative_url
-      req.headers = headers
-      req.params = params
+    def establish_post_connection(relative_url, params={})
+      conn = Faraday.new(base_url)
+
+      @response = conn.post do |req|
+        req.url relative_url
+        req.headers = headers
+        req.body = params
+      end
+    end
+
+    def establish_put_connection(relative_url, params={})
+      conn = Faraday.new(base_url)
+
+      @response = conn.put do |req|
+        req.url relative_url
+        req.headers = headers
+        req.body = params
+      end
+    end
+
+    def establish_delete_connection(relative_url, params={})
+      conn = Faraday.new(base_url)
+
+      @response = conn.delete do |req|
+        req.url relative_url
+        req.headers = headers
+        req.body = params
+      end
+    end
+
+    def response_data
+      JSON.parse(@response.body)
+    end
+
+    def error
+      (response_data["error"] || response_data["message"]).try(:humanize)
     end
   end
-
-  def establish_post_connection(relative_url, params={})
-    conn = Faraday.new(base_url)
-
-    @response = conn.post do |req|
-      req.url relative_url
-      req.headers = headers
-      req.body = params
-    end
-  end
-
-  def establish_put_connection(relative_url, params={})
-    conn = Faraday.new(base_url)
-
-    @response = conn.put do |req|
-      req.url relative_url
-      req.headers = headers
-      req.body = params
-    end
-  end
-
-  def establish_delete_connection(relative_url, params={})
-    conn = Faraday.new(base_url)
-
-    @response = conn.delete do |req|
-      req.url relative_url
-      req.headers = headers
-      req.body = params
-    end
-  end
-
-  def response_data
-    JSON.parse(@response.body)
-  end
-
-  def error
-    (response_data["error"] || response_data["message"]).try(:humanize)
-  end
-end
+end  
--- test_client.rb
+++ test_client.rb
@@ -8,13 +8,12 @@ end
 # ---- Source Type ----
 # search source_type by name
 service = EclDeveloperClient::SourceType.new(client_id,secret)
-service.get(name: "sociative_source")
+service.get(name: "sociative_source_1")
 service.response_data
-
 # create source type
 service = EclDeveloperClient::SourceType.new(client_id,secret)
 params = {
-  name: 'sample source type',
+  name: 'sample_source_type',
   display_name: 'Sample',
   is_singleton: true,
   is_public: true,
@@ -25,36 +24,22 @@ params = {
 }
 service.create(params)
 service.response_data
-
+binding.pry
 # update source type
-source_type_id = "a96138ca-e3ec-470c-a977-51e021d3e170"
+source_type_id = service.response_data["data"]["id"]
 service = EclDeveloperClient::SourceType.new(client_id,secret)
 service.update(source_type_id, {display_name: 'test display'})
 service.response_data
 
 # delete source type
-source_type_id = "a96138ca-e3ec-470c-a977-51e021d3e170"
-service = EclDeveloperClient::SourceType.new(client_id,secret)
-service.delete(source_type_id)
-service.response_data
+# source_type_id = "a96138ca-e3ec-470c-a977-51e021d3e170"
+# service = EclDeveloperClient::SourceType.new(client_id,secret)
+# service.delete(source_type_id)
+# service.response_data
 
-
-
-# ---- Source ----
-# Find source with id
-source_id = "01ed5cde-9190-49e3-8e0c-052d37c8a4de"
-service  = EclDeveloperClient::Source.new(client_id,secret)
-service.find(source_id)
-service.response_data
-
-# search source by source_config
-service = EclDeveloperClient::Source.new(client_id,secret)
-service.get(source_config: {"client_id"=>"libraryapi-client-ge",
- "client_secret"=>"22gU2EXvdVCXFUHrdhhG"})
-service.response_data
 
 # create source
-source_type_id = "a96138ca-e3ec-470c-a977-51e021d3e170"
+# source_type_id = "a96138ca-e3ec-470c-a977-51e021d3e170"
 service = EclDeveloperClient::Source.new(client_id,secret)
 params = {
   source_type_id: source_type_id,
@@ -67,11 +52,28 @@ params = {
 }
 service.create(params)
 service.response_data
+source_id = service.response_data["data"]["id"]
+
+
+# ---- Source ----
+# Find source with id
+# source_id = "01ed5cde-9190-49e3-8e0c-052d37c8a4de"
+service  = EclDeveloperClient::Source.new(client_id,secret)
+service.find(source_id)
+service.response_data
+
+# search source by source_config
+service = EclDeveloperClient::Source.new(client_id,secret)
+service.get(source_config: {"client_id"=>"libraryapi-client-ge",
+ "client_secret"=>"22gU2EXvdVCXFUHrdhhG"})
+service.response_data
+
+
 
 # update source
 source_id = "01ed5cde-9190-49e3-8e0c-052d37c8a4de"
 service = EclDeveloperClient::Source.new(client_id,secret)
-service.update(source_id, { display_name: 'test display'})
+service.update(source_id, { last_polled_at: Time.now})
 service.response_data
 
 # delete
@@ -82,7 +84,7 @@ service.response_data
 
 # trigger_collector
 source_id = "01ed5cde-9190-49e3-8e0c-052d37c8a4de"
-service = EclClient::Source.new(client_id,secret)
+service = EclDeveloperClient::Source.new(client_id,secret)
 service.trigger_collector(source_id)
 service.response_data
 
@@ -102,7 +104,7 @@ params = {
   source_id: source_id,
   source_type_id: source_type_id,
   external_id: "Test String",
-  is_public: boolean,
+  is_public: false,
   nlp_metadata: {},
   duration_metadata: {},
   content: "Test description",
@@ -111,32 +113,25 @@ params = {
   raw_record: {},
   taxonomies: {}
 }
-service = EclClient::ContentItem.new(client_id,secret)
+service = EclDeveloperClient::ContentItem.new(client_id,secret)
 service.create(params)
 service.response_data
 
 # get content items
-service  = EclClient::ContentItem.new(client_id,secret)
+service  = EclDeveloperClient::ContentItem.new(client_id,secret)
 service.get(limit: 30, offset: 1) # or service.get to get default
 service.response_data
 
 # find content item
 content_id = "007f8d65-410e-49d5-aa77-27ba87eb8576"
-service  = EclClient::ContentItem.new(client_id,secret)
+service  = EclDeveloperClient::ContentItem.new(client_id,secret)
 service.find(content_id)
-service.response_data
+puts service.response_data
 
 # delete content item
 content_id = "007f8d65-410e-49d5-aa77-27ba87eb8576"
-service  = EclClient::ContentItem.new(client_id,secret,{organization_id: 15})
+service  = EclDeveloperClient::ContentItem.new(client_id,secret,{organization_id: 15})
 service.delete(content_id)
-service.response_data
-
-# ---- Settings ----
-# get ecl settings for organization
-organization_id = 15
-service  = EclClient::EclSettings.new(client_id,secret)
-service.get(organization_id: organization_id)
-service.response_data
+puts service.response_data
 
 
