67396cfa42ceb85eff3292b41072085658bb79b4
Jash Kothari <40259699+jashkothari@users.noreply.github.com>
2020-03-02 18:53:36 +0530

    Update ecl_developer_client.rb
    Add relative path for langugage.

--- lib/ecl_developer_client.rb
+++ lib/ecl_developer_client.rb
@@ -9,6 +9,7 @@ require_relative "ecl_developer_client/source_type"
 require_relative "ecl_developer_client/source"
 require_relative "ecl_developer_client/content_item"
 require_relative "ecl_developer_client/version"
+require_relative "ecl_developer_client/language"
 
 module EclDeveloperClient
   # Your code goes here...
